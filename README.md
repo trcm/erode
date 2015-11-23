e0140cf45adab443ed722542053f21da8107ba50
Thomas Midson <tom@Thomass-MacBook-Pro-2.local>
2015-11-23 22:32:15 +1000

    added requst parsing and cmd line args

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Erode is a simple HTTP server written in Rust.
 
 Currently Erode is just a simple learning exercise to help me keep programming in my downtime and
 so I can learn the Rust language.
+
+## Todo
+
+- Connection handling
+- Request parsing
+- Everything else
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,82 @@
-// extern crate httparse;
+extern crate httparse;
+extern crate bufstream;
 
 use std::net::{TcpListener, TcpStream};
-use std::io::Read;
+use std::io::{Read, Write};
+// use std::io::Read;
 use std::thread;
+use std::process;
+use std::env;
 
+use bufstream::BufStream;
 // use httparse::Request;
 
-fn handle_client(stream: TcpStream) {
-    println!("connection");
-    let _ = stream.read(ref &mut [0; 128]);
 
-    // let mut headers = [httparse::EMPTY_HEADER; 16];
-    // let mut req = httparse::Request::new(&mut headers);
-    // let res = req.parse(&buff).unwrap();
+fn handle_client(stream: TcpStream) {
+    let addr = stream.peer_addr().unwrap();
+    let mut bs = BufStream::new(stream);
+    println!("Connection from {}", addr);
+
+    let mut buf = [0; 1024];
+    let mut path = "";
+    let mut method = "";
+    // stream.read(&mut buf);
+    let _ = bs.read(&mut buf);
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut req = httparse::Request::new(&mut headers);
+    let res = req.parse(&mut buf).unwrap();
+    if res.is_complete() {
+        if req.path.is_some() && req.method.is_some() {
+            match req.path {
+                Some(p) => {
+                    path = p;
+                },
+                None => {
+                    return;
+                }
+            }
+            match req.method {
+                Some(m) => {
+                    method = m;
+                },
+                None => {
+                    return;
+                }
+            }
+        }
+    }
+
+    if method.to_string().is_empty() || path.to_string().is_empty() {
+        return;
+    }
+    
+    send_response(bs.get_mut(), path, method);
+}
+
+fn send_response(stream: &mut TcpStream, path: &str, method: &str) {
+    let res = b"HTTP/1.0 200 OK\n \
+                Connection: close\n";
+    let _ = stream.write(res);
+}
+
+fn usage() {
+    println!("Usage: ./erode [host] [port]");
+    process::exit(0);
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
+    // grab the args
+    let a: Vec<_> = env::args().collect();
+
+    if a.len() != 3 {
+        usage();
+    }
+
+    // combine the strings into the addrress
+    let addr = a[1].to_string() + ":" + &a[2];
+
+    // TODO - Fix Panics from invalid addresses
+    let listener = TcpListener::bind(&*addr).unwrap();
 
     // accept connections and process them, spawning a new thread for each one
     for stream in listener.incoming() {
@@ -27,7 +87,7 @@ fn main() {
                 });
             }
             Err(e) => {
-                println!("connection failed");
+                println!("Connection failed: {}", e);
             }
         }
     }
