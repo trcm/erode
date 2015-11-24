6c6b6032429024e5af3ba06fadef5cee69fae00e
Thomas Midson <tom@Thomass-MacBook-Pro-2.local>
2015-11-25 00:25:37 +1000

    cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ extern crate time;
 
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
-// use std::io::Read;
 use std::fs::File;
 use std::thread;
 use std::process;
@@ -13,21 +12,46 @@ use std::env;
 
 use bufstream::BufStream;
 use getopts::Options;
-// use httparse::Request;
 
 
+/// Erode
+///
+/// Erode is a Toy http server I wrote to help me learn Rust.
+/// It is based off of a HTTP Server I have previously written for a university assignment.
+///
+/// Erode accepts the following arguments:
+///  Required -
+///  -a: address to serve the server on 
+///  -p: port to listen on 
+///  Options -
+///  -d: daemonize the server
+///  -l: toggle logging to the command line
+///  -h: prints the help text
+
+
+/// handle_client
+/// Handling an incomming connection to the server
+///
+/// Arguments:
+/// stream - Incoming TcpStream
 fn handle_client(stream: TcpStream) {
+
     let addr = stream.peer_addr().unwrap();
-    let mut bs = BufStream::new(stream);
     println!("Connection from {}", addr);
+
+    // setup aa new bufferedsteram for easier reading (buffered streams allow you to read by line)
+    let mut bs = BufStream::new(stream);
 
     let mut buf = [0; 1024];
     let mut path = "";
     let mut method = "";
-    // stream.read(&mut buf);
+
+    // read the request from the stream
     let _ = bs.read(&mut buf);
     let mut headers = [httparse::EMPTY_HEADER; 16];
     let mut req = httparse::Request::new(&mut headers);
+
+    // parse the request
     let res = req.parse(&mut buf).unwrap();
 
     if res.is_complete() {
@@ -48,15 +72,22 @@ fn handle_client(stream: TcpStream) {
         let _ = bs.write(res);
         return;
     }
-    
-    send_response(bs.get_mut(), path, method);
+
+    send_response(bs.get_mut(), path);
 }
 
-fn send_response(stream: &mut TcpStream, path: &str, method: &str) {
+/// send_response:
+/// will resolve the client request and response accordingly
+///
+/// Arguments
+/// stream - mutable reference to the client TcpStream
+/// path   - path to the file the user wants
+fn send_response(stream: &mut TcpStream, path: &str) {
 
-    // check if the file exists
     let trimmed = path.trim_left_matches('/');
     println!("User requsted {}", trimmed);
+
+    // check if the file exists
     let mut file = match File::open(trimmed) {
         Ok(file) => { file },
         Err(e) => {
@@ -68,35 +99,38 @@ fn send_response(stream: &mut TcpStream, path: &str, method: &str) {
         }
     };
 
+    // read file into a new vector
     let mut contents = Vec::new();
     let _ = file.read_to_end(&mut contents);
-    println!("{}", contents.len());
 
+    // construct response for the client
     let now = time::now();
-    
     let res = format!("HTTP/1.0 200 OK\n\
                        Date: {}\n\
                        Content-Length: {}\n\
                        Connection: close\n\r\n", now.ctime(), contents.len());
-    let _ = stream.write(&*res.into_bytes());
 
+    // send the header and the data
+    let _ = stream.write(&*res.into_bytes());
     let _ = stream.write(&*contents);
+   
+    // ensure all data has been sent to the client
     stream.flush().unwrap();
 }
 
+/// usage
+/// Simple usage printer that will be called in the case of invalid arguments
 fn usage() {
     println!("Usage: ./erode [-dlh] -h HOST -p PORT");
     process::exit(0);
 }
 
+/// Main
+///
 fn main() {
-    // grab the args
-    let args: Vec<_> = env::args().collect();
-    // let program = args[0].clone();
-    // if a.len() != 3 {
-    //     usage();
-    // }
 
+    // grab and parse the arguments
+    let args: Vec<_> = env::args().collect();
     let mut opts = Options::new();
     opts.optflag("d", "daemon", "Daemonize the server");
     opts.optflag("l", "log", "Turn on logging to the console");
@@ -104,25 +138,33 @@ fn main() {
     opts.reqopt("p", "port", "Local port to bind to", "PORT");
     opts.optflag("h", "help", "Print help");
 
+    //parse command line arguemnts
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
         Err(f) => { panic!(f.to_string()) }
     };
+
     if matches.opt_present("h") {
         usage();
         return;
     }
 
+    // combine the strings into the addrress
     let address = matches.opt_str("a").unwrap();
     let port = matches.opt_str("p").unwrap();
-    
 
-    // combine the strings into the addrress
-    // let addr = args[1].to_string() + ":" + &args[2];
     let addr = address.to_string() + ":" + &port;
 
-    // TODO - Fix Panics from invalid addresses
-    let listener = TcpListener::bind(&*addr).unwrap();
+    // Bind a new TCP Socket to the given address
+    let listener = match TcpListener::bind(&*addr) {
+        Ok(listener) => {
+            listener
+        },
+        Err(e) => {
+            println!("Error: {}", e);
+            return;
+        }
+    };
 
     // accept connections and process them, spawning a new thread for each one
     for stream in listener.incoming() {
@@ -137,6 +179,7 @@ fn main() {
             }
         }
     }
+
     // close the socket server
     drop(listener);
 }
